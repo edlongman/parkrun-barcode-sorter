7eb96265b713b79c98d60fd147e70e2c673f7acb
Edward Longman <eialongman@gmail.com>
2021-11-21 21:26:04 +0000

    Create set reply mechanism for serial port

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
+#include <string.h>
 #include "FlapGovenor.h"
 #include "MotorController.h"
 #include "ConsoleUart.h"
+
+const uint8_t buff_size = 16;
 
 int main(void){
     FlapGovenor::init();
@@ -11,10 +14,18 @@ int main(void){
     MotorController::init();
     MotorController::enable();
     ConsoleUart::init();
-    char buffer[16] = "Hello World";
+    ConsoleUart::enable();
+    char tx_buffer[buff_size] = "Hello World";
+    char rx_buffer[buff_size];
     sei();
+    ConsoleUart::scanLine(rx_buffer, buff_size);
     while(1){
-        ConsoleUart::blockingSendLine(buffer, 16);
+        ConsoleUart::blockingSendLine(tx_buffer, buff_size);
         _delay_ms(500);
+        if(ConsoleUart::isScanComplete()){
+            // Copy into tx buffer and setup next scanning
+            memcpy(tx_buffer, rx_buffer, buff_size);
+            ConsoleUart::scanLine(rx_buffer, buff_size);
+        }
     }
 }
